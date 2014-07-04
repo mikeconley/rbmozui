b7db70d157b77bcb36c54992cc22fcfef3cabf80
Mike Conley <mike.d.conley@gmail.com>
2014-07-03 21:07:57 -0400

    Closing the repo.

--- rbmozui/templatetags/rbmozui.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter()
-def isSquashed(aReviewRequest):
-    return aReviewRequest.extra_data.get('p2rb.is_squashed', 'False') == 'True'
